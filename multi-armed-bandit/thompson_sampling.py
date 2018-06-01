a9c1ed957600317d5628b8c696d23ea782a1e00d
Luis Leal <wichofer89@gmail.com>
2018-05-31 23:27:04 -0600

    fixed some bugs in BayesianBandit

--- multi-armed-bandit/thompson_sampling.py
+++ multi-armed-bandit/thompson_sampling.py
@@ -5,20 +5,17 @@ class BayesianBandit():
 	def __init__(self,true_reward_mean):
 		self.true_reward_mean = true_reward_mean
 
-		self.initial_mean = 0 #m0
-		self.predicted_mean = initial_mean #m
-		self.initial_lambda_ = 1 #initial std
+		self.predicted_mean = 0
 		self.lambda_  = initial_lambda_ #estimated std
 		
-		self.sum_x = 0 #aux variable to calculate the gausian parameters(mean and std)
-		self.tau = self.lambda_ #used in the calculation of gausian parameters
-		self.N = 0
+		self.sum_x = 1 #aux variable to calculate the gausian parameters(mean and std)
+		self.tau = 1 #used in the calculation of gausian parameters
+		
 
 	def pull(self):
-		self.N+=1
 		return np.random.randn() + self.true_reward_mean
 
-	def sample(self): 
+	def sample_mean_distribution(self): 
 		'''	when selecting the best bandit instead of selecting  the one with biggest estimated mean
 			we sample a mean from the estimated mean gaussian distrubution and select the bandit with biggest sampled mean
 		'''
@@ -26,6 +23,6 @@ class BayesianBandit():
 
 	def update(self,x):
 
-		self.lambda_  = self.initial_lambda_ + (self.tau*self.N)
+		self.lambda_  += self.tau
 		self.sum_x += x
-		self.predicted_mean = (self.initial_lambda_*self.initial_mean +self.tau*self.sum_x)/(self.initial_lambda_  + self.tau*self.N)
+		self.predicted_mean = self.tau*self.sum_x / self.lambda_
