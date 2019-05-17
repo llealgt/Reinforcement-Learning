620b9915bd866c93bda6cf52a29963f5cbc2f4e0
Luis Leal <llealhernandez@paypal.com>
2019-05-16 19:14:41 -0600

    Added experiment with OpenAI taxiv2 environment with 9.3 average reward result

---
+++ temporal_difference_learning/OpenAI_TaxiV2_QLearning/agent.py
@@ -0,0 +1,49 @@
+import numpy as np
+from collections import defaultdict
+
+class Agent:
+
+    def __init__(self, nA=6):
+        """ Initialize agent.
+
+        Params
+        ======
+        - nA: number of actions available to the agent
+        """
+        self.nA = nA
+        self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 0.0
+        self.gamma = 0.5
+        self.alpha = 0.1
+        self.episode = 1
+
+    def select_action(self, state):
+        """ Given the state, select an action.
+
+        Params
+        ======
+        - state: the current state of the environment
+
+        Returns
+        =======
+        - action: an integer, compatible with the task's action space
+        """
+        action = np.random.choice(self.nA) if np.random.random() <= self.epsilon else np.argmax(self.Q[state])
+        return action
+        #return np.random.choice(self.nA)
+
+    def step(self, state, action, reward, next_state, done):
+        """ Update the agent's knowledge, using the most recently sampled tuple.
+
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+        #self.Q[state][action] += 1
+        self.Q[state][action] = self.Q[state][action] + self.alpha*(reward + self.gamma*np.max(self.Q[next_state]) - self.Q[state][action])
+        self.epsilon = 1/self.episode
+        self.episode += 1
